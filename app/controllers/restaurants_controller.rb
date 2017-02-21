aef99e321d1bc217b63baed1415245ad9c5d0ce5
Ross Benzie <rossbenzie@gmail.com>
2017-02-21 22:21:20 +0000

    CRUD complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,41 @@
 class RestaurantsController < ApplicationController
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+    # params.require(:restaurant).permit(:description)
+    # params.require(:restaurant).permit(:id)
+  end
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to '/restaurants'    
   end
 end
