4c699a9b1f849dbedbe83cb405c21099cf3175fb
Enrico Graziani <mrenrich84@gmail.com>
2017-02-22 15:55:58 +0000

    Add sign_up and sign_in to web_helper

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -12,16 +12,26 @@ feature "User can sign in and out" do
       visit('/')
       expect(page).not_to have_link('Sign out')
     end
+
+    it "user can sign out" do
+      sign_up
+      click_link('Sign out')
+      expect(page).not_to have_link('Sign out')
+      expect(page).to have_link('Sign in')
+      expect(page).to have_link('Sign up')
+    end
+
+    it "user can sign in" do
+      sign_up
+      click_link('Sign out')
+      sign_in
+      expect(page).to have_link('Sign out')
+    end
   end
 
   context "user signed in on the homepage" do
     before do
-      visit('/')
-      click_link('Sign up')
-      fill_in('Email', with: 'test@example.com')
-      fill_in('Password', with: 'testtest')
-      fill_in('Password confirmation', with: 'testtest')
-      click_button('Sign up')
+      sign_up
     end
 
     it "should see 'sign out' link" do
