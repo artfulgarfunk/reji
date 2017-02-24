b7e8af192ee7461635f0731b8cd509d5e118e0d5
Irene Canuti <irene.canuti@gmail.com>
2017-02-24 15:18:22 +0000

    Update test for img upload

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -30,8 +30,7 @@ feature 'restaurants' do
       click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
-      # it works but the syntax is not updated.
-      page.status_code.should == 200
+      expect(page).to have_http_status(200)
     end
 
     scenario 'cannot create restaurants if not logged in' do
